77f2725ed126ad4f2944fb9121af4c3afa968854
A-GustavoStori <aldrinstori@gmail.com>
2024-07-23 23:12:56 -0300

    * Data for calisthenics page

--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -1,4 +1,3 @@
-
 import { Checkbox } from 'antd';
 import { CheckOutlined } from '@ant-design/icons';
 import { Navbar } from '@/components/navbar-menu'; 
@@ -14,19 +13,18 @@ export default function Projects() {
             All projects
           </h1>
         </header>
-        
 
          {/* Projects */}
          <section id="about" className="w-full max-w-5xl mb-12">
           <p>More than 20 software projects</p>
           <span className="block my-6"></span>
 
-         <ul className="list-disc pl-6 text-[#E6F3FF]">
+          <ul className="list-disc pl-6 text-[#E6F3FF]">
+          <li>Portal do Candidato</li>
             <li>Free Youtube Summarizer</li>
             <li>Wisdom Collector</li>
             <li>Preta Fina Acessórios</li>
             <li>Workspace Manager</li>
-            <li>Portal do Candidato</li>
             <li>Exam ScoreSystem</li>
             <li>Faul</li>
             <li>FunTechy</li>
@@ -40,13 +38,13 @@ export default function Projects() {
             <li>Exam Allocator</li>
             <li>Avaya Toolbar</li>
             <li>FUNTEF AI</li>
-        </ul>
-        <span className="block my-6"></span>
+          </ul>
+          <span className="block my-6"></span>
 
           <h2 className="text-2xl font-semibold mb-4 text-white drop-shadow-[0_1px_1px_rgba(0,255,208,0.8)]">
             Portal do candidato 
           </h2>
-          <p className="text-lg text-[#E6F3FF]">
+          <p className="text-lg text-[#E6F3FF] mb-6">
             As a Technical Leader, i led the development team and implementation of the digital registration and examination system for the Universidade Tecnológica Federal do Paraná's (UTFPR) 
             Entrance Exam since 2023. Successfully streamlined the registration process for over 3 years, across 114 courses, enabling a smooth, error-free registration experience for applicants. 
             Innovated by integrating a fee exemption feature, quota reservation system, and reasonable adaptations module for candidates with specific needs, significantly enhancing the inclusivity and efficiency of the admissions process.
@@ -57,36 +55,38 @@ export default function Projects() {
 
             The software is still being used by UTFPR and FUNTEFPR.
           </p>
+          <span className="block my-6"></span>
 
           <h2 className="text-2xl font-semibold mb-4 text-white drop-shadow-[0_1px_1px_rgba(0,255,208,0.8)]">
             FUNTEFPR AI
           </h2>
-          <p className="text-lg text-[#E6F3FF]">
+          <p className="text-lg text-[#E6F3FF] mb-6">
             AI developed with RAG to increase collaborators performance and quicker assistance to clients. Instead of having  to manually search over 5 terabytes of documents the AI makes a auto digest in the Drive documents
-            and stores it on a vector database, so when the user needs that data the AI searches for the user, also gives IT support and works as Knowledge base. Which decreased 90% of the call time, 
-
-            <h6>Text and voice search</h6> 
-            User assistance
-            Client assistance
-            Knowledgebase
-            IT Support
+            and stores it on a vector database, so when the user needs that data the AI searches for the user, also gives IT support and works as Knowledge base. Which decreased 90% of the call time.
           </p>
+          <ul className="list-disc pl-6 text-[#E6F3FF] mb-6">
+            <li>Text and voice search</li> 
+            <li>User assistance</li>
+            <li>Client assistance</li>
+            <li>Knowledgebase</li>
+            <li>IT Support</li>
+          </ul>
 
           <h2 className="text-2xl font-semibold mb-4 text-white drop-shadow-[0_1px_1px_rgba(0,255,208,0.8)]">
             Portal do colaborador 
           </h2>
-          <p className="text-lg text-[#E6F3FF]">
+          <p className="text-lg text-[#E6F3FF] mb-6">
             As a Technical Leader, I led the development team and implementation of the digital registration and examination system for the Universidade Tecnológica Federal do Paraná's (UTFPR) 
-            Entrance Exam since 2023. Successfully streamlined the registration process for over 3 years, across 114 courses, enabling a smooth, error-free registration experience for applicants.\n\nInnovated by integrating a 
-            fee exemption feature, quota reservation system, and reasonable adaptations module for candidates with specific needs, significantly enhancing the inclusivity and efficiency of the admissions process.\n\n
-            The system was so well received that UTFPR used it for public exams to select candidates to be federal employees of the university.\n\nSystem designed for candidate registration for UTFPR and FUNTEFPR selective processes,
-            including university entrance exams and public competitions.\n\n
+            Entrance Exam since 2023. Successfully streamlined the registration process for over 3 years, across 114 courses, enabling a smooth, error-free registration experience for applicants. Innovated by integrating a 
+            fee exemption feature, quota reservation system, and reasonable adaptations module for candidates with specific needs, significantly enhancing the inclusivity and efficiency of the admissions process.
+            The system was so well received that UTFPR used it for public exams to select candidates to be federal employees of the university. System designed for candidate registration for UTFPR and FUNTEFPR selective processes,
+            including university entrance exams and public competitions.
 
-            Over $4 million was collected and more than 150,000 unique registered candidates with over 1 million accesses.\n\n
+            Over $4 million was collected and more than 150,000 unique registered candidates with over 1 million accesses.
+
             The software is still being used by UTFPR and FUNTEFPR.
           </p>
         </section>
-
 
         {/* Footer */}
         <footer className="w-full max-w-5xl mt-12 text-center text-[#E6F3FF]">
